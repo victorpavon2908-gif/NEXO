d5d427795939fdd9291cc2afa48aedee9c4db5d6
victorpavon2908-gif <victorpavon2908@gmail.com>
2026-09-08 11:02:12 -0600

    fix: use Android Gradle Plugin 9.2 for wider Studio compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "9.4.0" apply false
+    id("com.android.application") version "9.2.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.4.10" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.4.10" apply false
 }
