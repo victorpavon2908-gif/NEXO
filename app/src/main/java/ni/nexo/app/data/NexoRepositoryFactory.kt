9da4dee2440aeb1a5aeb49be12d93bcbc9d8131e
victorpavon2908-gif <victorpavon2908@gmail.com>
2026-09-08 16:56:23 -0600

    feat: enable RC3 advanced communication repository

--- app/src/main/java/ni/nexo/app/data/NexoRepositoryFactory.kt
+++ app/src/main/java/ni/nexo/app/data/NexoRepositoryFactory.kt
@@ -3,6 +3,10 @@ package ni.nexo.app.data
 object NexoRepositoryFactory {
     fun create(): NexoRepository {
         val client = SupabaseClientProvider.client
-        return if (client != null) SupabaseNexoRepository(client) else DemoNexoRepository()
+        return if (client != null) {
+            AdvancedNexoRepository(SupabaseNexoRepository(client), client)
+        } else {
+            DemoNexoRepository()
+        }
     }
 }
