33a989992b8a8efb20ad4392764731f6164060ed
victorpavon2908-gif <victorpavon2908@gmail.com>
2026-09-10 17:00:32 -0600

    feat: add Firebase Google Services plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,4 +8,5 @@ plugins {
     id("com.android.application") version "9.2.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.4.10" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.4.10" apply false
+    id("com.google.gms.google-services") version "4.5.0" apply false
 }
